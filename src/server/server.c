20dd0c1ac05ba02299e45bb8958404b9012355a8
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2024-12-29 12:30:05 +0100

    Gracefully stop server

--- src/server/server.c
+++ src/server/server.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <signal.h>
 #include <string.h>
+#include <stdlib.h>
 #include "server.h"
 #include "common.h"
 
 void stopServer(int _)
 {
-	if(!stopserver()) printf("Failed to gracefully stop the server\n");
+	int error = stopserver();
+	if (error)
+		printf("Failed to gracefully stop the server\n");
+	exit(error);
 }
 
 int main(int argc, char **argv)
 {
 	printf("Starting server\n");
-	// signal(SIGINT, stopServer);
+	signal(SIGINT, stopServer);
 	startserver(SERVER_PORT);
 
-	MESSAGE *message;
-	while ((message = read_full_message()))
+	while (1)
 	{
+		MESSAGE *message = read_full_message();
 		printf("Received [%c]: %s\n", message->action_type, message->content);
 		free(message);
 	}
-	return 0;
 }
